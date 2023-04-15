5f8d889876cb5331ee20e82362044b45d11bd032
skynet <wdx_skynet@yeah.net>
2023-04-15 16:22:02 +0800

    none

--- src/main.js
+++ src/main.js
@@ -2,5 +2,8 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
+import ElementPlus from "element-plus";
+import "element-plus/lib/theme-chalk/index.css";
 
+createApp(App).use(ElementPlus).mount("#app");
 createApp(App).use(router).mount("#app");
